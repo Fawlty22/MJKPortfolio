11918c87d35d6d2bfed3cab445e468445a64a428
Matt Keys <MatthewjKeys22@gmail.com>
2022-09-15 17:11:32 -0400

    inital commit with files and home section complete

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { useCallback } from "react";
+import Particles from "react-particles";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import { loadFull } from "tsparticles";
+import Navbar from "./components/Navbar"
+import Home from "./components/Home/Home";
+import Footer from "./components/Footer";
+import logo from "./logo.svg";
+import "./style.css"
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import particlesOptions from "./particles.json";
+
+
+function App() {
+  const particlesInit = useCallback((main) => {
+    loadFull(main);
+  }, []);
+
+  return (
+    <Router>
+      <div className="App">
+        <Particles options={particlesOptions} init={particlesInit} />
+        <Navbar />
+        <Routes>
+          <Route path="/portfolio" exact element={<Home />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Home/Home2.js
@@ -0,0 +1,100 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import myImg from "../../assets/avatar.svg";
+import Tilt from "react-parallax-tilt";
+import {
+  AiFillGithub,
+  AiFillInstagram
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function Home2() {
+  return (
+    <Container fluid className="home-about-section" id="about">
+      <Container>
+        <Row>
+          <Col md={8} className="home-about-description">
+            <h1 style={{ fontSize: "2.6em" }}>
+              LET ME <span className="purple"> INTRODUCE </span> MYSELF
+            </h1>
+            <p className="home-about-body">
+            I currently reside in Beacon, NY, after recently receiving a certificate in Full Stack Web Development from the University of Utah.
+              <br />
+              <br />I am most familiar with 
+              <i>
+                <b className="purple"> Javascript ES6+ and the MERN stack </b>
+              </i>
+              , though I do have experience with other technology stacks which you can find on my resume, linked above.  
+              <br />
+              <br />
+              In my free time, I am an avid &nbsp;
+              <i>
+                <b className="purple">disc golfer and skier. </b> 
+              </i>
+              I am also a huge Magic: the Gathering nerd. But when im in the office, I enjoy
+                <i><b className="purple"> learning new technologies and building projects</b></i>
+              <br />
+              <br />
+              Potential future employers and new friends alike, please feel free to contact me by pressing the button below. My
+              <i>
+                <b className="purple">
+                  {" "}
+                  LinkedIn, Github, and Instagram{" "}
+                </b>
+              </i>
+              are linked at the top & bottom of the page.
+              &nbsp; 
+              
+            </p>
+          </Col>
+          <Col md={4} className="myAvtar">
+            <Tilt>
+              <img src={myImg} className="img-fluid" alt="avatar" />
+            </Tilt>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} className="home-about-social">
+            <h1>FIND ME ON</h1>
+            <p>
+              Feel free to <span className="purple">connect </span>with me
+            </p>
+            <ul className="home-about-social-links">
+              <li className="social-icons">
+                <a
+                  href="https://github.com/Fawlty22"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiFillGithub />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.linkedin.com/in/matthewkeysprofile/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <FaLinkedinIn />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.instagram.com/matt_keys/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour home-social-icons"
+                >
+                  <AiFillInstagram />
+                </a>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+export default Home2;
