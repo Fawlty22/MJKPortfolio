eb55876572c2e435d026a325fec93f52247f5917
Matt Keys <MatthewjKeys22@gmail.com>
2022-09-16 12:38:15 -0400

    resume file upload, correct pathing, resume section done

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -5,11 +5,12 @@ import pdf from "../../assets/Resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import PDF from "../../assets/Resume.pdf"
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/Fawlty22/MJKPortfolio2/master/src/assets/Resume.pdf";
-
+  "https://raw.githubusercontent.com/Fawlty22/MJKPortfolio2/src/assets/Resume.pdf";
+  
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
 
@@ -28,7 +29,7 @@ function ResumeNew() {
         </Row>
 
         <Row className="resume">
-          <Document file={resumeLink}>
+          <Document file={PDF}>
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
         </Row>
